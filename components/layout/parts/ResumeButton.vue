d864f42c83a527fde8e8ef164ea0d00c94607bd5
Goudmane Oualid <walidlioon.09@gmail.com>
2024-11-05 18:57:22 +0100

    fix mobile display

---
+++ components/layout/parts/ResumeButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <a v-if="!isLoading" :href="resumeLink" class="resume-button" target="_blank" rel="noopener noreferrer" download>
+        {{ $t('lang.resume') }}
+    </a>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    isLoading: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const { locale } = useI18n();
+const resumeLink = computed(() => `/static/pdf/Goudmane Oualid ${locale.value}.pdf`);
+</script>
+
+<style scoped>
+</style>
